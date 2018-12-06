4e2cef1143cf076a71c1eb8080b2bae419829859
Nick Bansal <https://github.com/NickBansal>
2018-12-06 12:09:56 +0000

    Draw a circle onto canvas

--- breakout.js
+++ breakout.js
@@ -7,3 +7,10 @@ ctx.rect(0, 300, 80, 20);
 ctx.fillStyle = "#FFEECC";
 ctx.fill();
 ctx.closePath();
+
+// Draw a Circle
+ctx.beginPath();
+ctx.arc(240, 160, 10, 0, Math.PI*2, false);
+ctx.fillStyle = "#CCCCCC";
+ctx.fill();
+ctx.closePath();
