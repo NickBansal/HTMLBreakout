850bb8c17670ecfe7522e427790391bb05d11a59
Nick Bansal <https://github.com/NickBansal>
2018-12-06 12:21:24 +0000

    Ball moving

--- breakout.js
+++ breakout.js
@@ -2,15 +2,27 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 // Draw a rectangle
-ctx.beginPath();
-ctx.rect(0, 300, 80, 20);
-ctx.fillStyle = "#FFEECC";
-ctx.fill();
-ctx.closePath();
+// ctx.beginPath();
+// ctx.rect(0, 300, 80, 20);
+// ctx.fillStyle = "#FFEECC";
+// ctx.fill();
+// ctx.closePath();
 
-// Draw a Circle
-ctx.beginPath();
-ctx.arc(240, 160, 10, 0, Math.PI*2, false);
-ctx.fillStyle = "#CCCCCC";
-ctx.fill();
-ctx.closePath();
+let x = canvas.width/2;
+let y = canvas.height-30; 
+
+let dx = 2;
+let dy = -2;
+
+const draw = () => {
+    // Draw a Circle
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = "#CCCCCC";
+    ctx.fill();
+    ctx.closePath();
+    x += dx;
+    y += dy;
+}
+
+setInterval(draw, 10);
