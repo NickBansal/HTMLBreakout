555127f47730b5498db3435b6f29139660f32927
Nick Bansal <https://github.com/NickBansal>
2018-12-06 12:52:07 +0000

    Added paddle movement

--- breakout.js
+++ breakout.js
@@ -1,28 +1,55 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-// Draw a rectangle
-// ctx.beginPath();
-// ctx.rect(0, 300, 80, 20);
-// ctx.fillStyle = "#FFEECC";
-// ctx.fill();
-// ctx.closePath();
-
 let x = canvas.width/2;
-let y = canvas.height-30; 
-
-let dx = 2;
+let y = canvas.height/2; 
+let ballRadius = 10;
+let dx = -2;
 let dy = -2;
+let paddleHeight = 20;
+let paddleWidth = 80;
+let paddleX = (canvas.width-paddleWidth)/2;
+let rightPressed = false;
+let leftPressed = false;
 
-const draw = () => {
-    // Draw a Circle
+const drawBall = () => {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = "#CCCCCC";
     ctx.fill();
     ctx.closePath();
-    x += dx;
-    y += dy;
 }
 
+const drawRectangle = () => {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight)
+    ctx.fillStyle = "#FFEECC";
+    ctx.fill();
+    ctx.closePath();
+}
+
+const draw = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    drawRectangle();
+    x += dx;
+    y += dy;
+    if (x + dx > canvas.width-ballRadius || x + dx < ballRadius) dx = -dx;
+    if (y + dy > canvas.height-ballRadius || y + dy < ballRadius) dy = -dy;
+    if (rightPressed && paddleX < canvas.width-paddleWidth) paddleX += 7;
+    if (leftPressed && paddleX > 0) paddleX -= 7;
+}
+
+const keyDownHandler = (e) => {
+    if (e.keyCode == 39) rightPressed = true
+    else if (e.keyCode == 37) leftPressed = true;
+}
+
+const keyUpHandler = (e) => {
+    if (e.keyCode == 39) rightPressed = false
+    else if (e.keyCode == 37) leftPressed = false;
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 setInterval(draw, 10);
