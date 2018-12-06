8eed255c85e324f7330d07b3ddadf3513978f030
Nick Bansal <https://github.com/NickBansal>
2018-12-06 12:06:32 +0000

    Draw a rectangle onto canvas

--- breakout.js
+++ breakout.js
@@ -1,7 +1,7 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-
+// Draw a rectangle
 ctx.beginPath();
 ctx.rect(0, 300, 80, 20);
 ctx.fillStyle = "#FFEECC";
